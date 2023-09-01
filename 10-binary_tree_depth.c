4397c493fc404671e95c26885436752f4103ca81
abdelrahmanmousa <abdelrahmanmousa33@gmail.com.com>
2023-09-01 08:53:14 -0700

    task 10 improved

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,7 +9,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
 
-	if (tree->parent != NULL)
+	if (tree->parent && tree)
 	{
 		depth = binary_tree_depth(tree->parent);
 		return (depth + 1);
